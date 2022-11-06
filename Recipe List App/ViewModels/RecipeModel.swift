8e54dfef69fd4e212efe21d23d78c3a66b9ba550
TeaTea <93224542+tranthanhhh@users.noreply.github.com>
2022-11-06 16:51:09 -0600

    - Add GCD func - Add getPortion func - UI improvement

--- Recipe List App/ViewModels/RecipeModel.swift
+++ Recipe List App/ViewModels/RecipeModel.swift
@@ -15,4 +15,68 @@ class RecipeModel:ObservableObject {
         self.recipes = DataService.getLocalData()
     }
     
+    static func getPortion(ingredient:Ingredient, recipeServings: Int, targetServings: Int) -> String {
+        
+        var portion = ""
+        var numerator = ingredient.num ?? 1
+        var denominator = ingredient.denom ?? 1
+        var wholePortions = 0
+        
+        if ingredient.num != nil {
+            //Get a single serving size by multiplying denom by the recipe serving
+            denominator *= recipeServings
+            
+            //Get target portion by multiplying numerator by target serving
+            numerator *= targetServings
+            
+            // Reduce fraction by GCD
+            let divisor = Rational.greatestCommonDivisor(numerator, denominator)
+            numerator /= divisor
+            denominator /= divisor
+            
+            //Get the whole portion if num > denom
+            if numerator >= denominator {
+                wholePortions = numerator / denominator
+                
+            //Remainder
+                numerator = numerator % denominator
+                
+            //Assign to portion string
+                portion += String(wholePortions)
+            }
+            
+            
+            //Express the remainder as a fraction
+            if numerator > 0 {
+                //Assign remainder as fraction to the portion string
+                portion += wholePortions > 0 ? " " : ""
+                portion += "\(numerator)/\(denominator)"
+            }
+        }
+        
+        if var unit = ingredient.unit {
+
+            //Prulalize
+            if wholePortions > 1 {
+                //Calculate suffix
+                if unit.suffix(2) == "ch" {
+                    unit += "es"
+                }
+                else if unit.suffix(1) == "f" {
+                    unit = String(unit.dropLast())
+                    unit += "ves"
+                }
+                else {
+                    unit += "s"
+                }
+            }
+            
+            portion += ingredient.num == nil && ingredient.denom == nil ? "" : " "
+            
+            return portion + unit
+        }
+        
+        return portion
+    }
+    
 }
--- Recipe List App/Views/RecipeDetailView.swift
+++ Recipe List App/Views/RecipeDetailView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct RecipeDetailView: View {
     
     var recipe:Recipe
+    @State var selectedServingSize = 2
     
     var body: some View {
         ScrollView {
@@ -25,17 +26,33 @@ struct RecipeDetailView: View {
                     .scaledToFill()
                 
                 
+                //MARK: Serving Size Picker
+                VStack(alignment: .leading) {
+                    Text("Select your serving style")
+                    Picker("", selection: $selectedServingSize) {
+                        Text("2").tag(2)
+                        Text("4").tag(4)
+                        Text("6").tag(6)
+                        Text("8").tag(8)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .frame(width: 160)
+                }
+                .padding()
+                
                 //MARK: Ingredients
                 VStack (alignment: .leading){
                     Text("Ingredients")
                         .font(.headline)
                         .padding([.bottom,.top], 5)
                     ForEach (recipe.ingredients) { item in
-                        Text("- " + item.name)
+                        Text("- " + RecipeModel.getPortion(ingredient: item, recipeServings: recipe.servings, targetServings: selectedServingSize) + " " + item.name.lowercased())
                     }
                 }
                 .padding(.horizontal)
                 Divider()
+                
+                
                 //MARK: Direction
                 VStack(alignment: .leading) {
                     Text("Directions")
